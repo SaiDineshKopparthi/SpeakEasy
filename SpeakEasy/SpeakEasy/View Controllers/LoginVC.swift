0b278b19aa3133a85287b8d4fe3d85b7d8d78294
Kopparthi Sai Dinesh <kopparthi.dinesh42224@gmail.com>
2023-10-30 22:59:15 -0500

    Login Page Layout created.

---
+++ SpeakEasy/SpeakEasy/View Controllers/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  SpeakEasy
+//
+//  Created by Kopparthi,Sai Dinesh on 10/30/23.
+//
+
+import UIKit
+import Lottie
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var logoAnimation: LottieAnimationView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.logoAnimation.animation = LottieAnimation.named("Animation_LoginScreen")
+        self.logoAnimation.loopMode = .loop
+        self.logoAnimation.play()
+    }
+
+}
