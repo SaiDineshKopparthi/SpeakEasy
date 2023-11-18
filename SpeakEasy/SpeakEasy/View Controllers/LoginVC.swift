a8cf1cbe55889115f0bdb7efe41d64d0eff7978a
Kopparthi Sai Dinesh <kopparthi.dinesh42224@gmail.com>
2023-11-18 00:14:09 -0600

    Sai Dinesh Kopparthi: Re-Constructed Login Screen Auto Layout

--- SpeakEasy/SpeakEasy/View Controllers/LoginVC.swift
+++ SpeakEasy/SpeakEasy/View Controllers/LoginVC.swift
@@ -11,15 +11,11 @@ import Lottie
 class LoginVC: UIViewController {
     
     @IBOutlet weak var logoAnimation: LottieAnimationView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.logoAnimation.animation = LottieAnimation.named("Animation_LoginScreen")
         self.logoAnimation.loopMode = .loop
         self.logoAnimation.play()
-    }
-    @IBAction func login(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "loginToApp", sender: nil)
     }
     
 }
